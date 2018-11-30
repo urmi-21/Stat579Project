fcf2218312c8a0862c40e7ff25653ba3a3a0749c
urmi-21 <mrbains21@gmail.com>
2018-11-30 01:27:03 -0600

    Update expression.R

--- expression.R
+++ expression.R
@@ -7,11 +7,14 @@ brca_tumor <- read_delim("brca-rsem-fpkm-tcga-t.txt", "\t", escape_double = FALS
 
 #formatting
 rn<-brca_nontumor$Hugo_Symbol
-row.names(brca_nontumor)<-brca_nontumor$Hugo_Symbol
 brca_nontumor<-brca_nontumor[,3:dim(brca_nontumor)[2]]
+rownames(brca_nontumor)<-rn
 
-rownames(brca_tumor)<-brca_tumor$Hugo_Symbol
+rn<-brca_tumor$Hugo_Symbol
 brca_tumor<-brca_tumor[,3:dim(brca_tumor)[2]]
+rownames(brca_tumor)<-rn
 
 #find correlation of tp53
-which(rownames(brca_nontumor)=="tp53")
+tp53data<-brca_nontumor[which(rownames(brca_nontumor)=="TP53"),]
+
+brca_nontumorCor<-cor(t(brca_nontumor))
