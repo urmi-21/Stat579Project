9e7c65522c8070965dc89888005a55d8cab94234
urmi-21 <mrbains21@gmail.com>
2018-12-03 20:42:47 -0600

    Update expression.R

--- expression.R
+++ expression.R
@@ -1,5 +1,7 @@
 library(readr)
 library(dplyr)
+library(ggplot2)
+library(reshape)
 #Read expression data
 brca_nontumor <- read_delim("brca-rsem-fpkm-tcga.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
 brca_tumor <- read_delim("brca-rsem-fpkm-tcga-t.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
@@ -21,3 +23,17 @@ brca_nontumorCor<-cor(t(brca_nontumor))
 
 x<-brca_nontumorCor[which(rownames(brca_nontumorCor)=="TP53"),]
 sort(x, decreasing = T)
+
+#plot box plots of top mutated genes
+topGenes<-c("PIK3CA","TP53","TTN","GATA3","CDH1","MAP3K1","MUC16","KMT2C","MUC4","PTEN")
+nt<-as.data.frame(t(brca_nontumor[topGenes,]))
+colnames(nt)<-topGenes
+
+ggplot(data=nt)+ geom_boxplot(stat = "count")
+
+ggplot(melt(nt), aes(x=factor(variable),y=value,fill=factor(variable)))+geom_boxplot()+scale_y_log10()+theme(legend.position = "top")+
+  theme(axis.text.x = element_text(angle=45,size = 15,face = "bold"),axis.text.y = element_text(size = 10,face = "bold"),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(), axis.title=element_text(size=12,face="bold"))+ scale_fill_discrete(name = "Gene")
+
